bc8e100f4db965779b98cf21971f459750704407
Murathanisk <114439297+Murathanisk@users.noreply.github.com>
2023-02-08 10:50:59 +0300

    Add files via upload

--- ArrayTask.java
+++ ArrayTask.java
@@ -4,7 +4,7 @@ public class ArrayTask {
     public static int sumEven(int[] a){ 
         int even = 0;
         for(int i = 0; i < a.length; i++){
-            if(a[i] % 2 == 0){
+            if(i % 2 == 0){
                 even += a[i];
             }
         }
@@ -14,11 +14,16 @@ public class ArrayTask {
     public static int sumOdd(int[] a){
         int odd = 0;
         for(int i = 0; i < a.length; i++){
-            if(a[i] % 2 != 0){
+            if(i % 2 != 0){
                 odd += a[i];
             }
         }
         return odd;
+    }
+
+    public static void main(String[] args) {
+        int[] a = {1,2,3,4,5};
+        System.out.println(sumEven(a));
     }
     
 }
